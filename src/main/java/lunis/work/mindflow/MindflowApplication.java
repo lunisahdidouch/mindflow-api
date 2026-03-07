c933ebb53c876aeb16b34bfc8f88c31e8df1edc7
Lunis <lunis1@live.nl>
2026-03-08 00:36:06 +0100

    Initial commit

---
+++ src/main/java/lunis/work/mindflow/MindflowApplication.java
@@ -0,0 +1,13 @@
+package lunis.work.mindflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lunis/work/mindflow/MindflowApplicationTests.java
@@ -0,0 +1,13 @@
+package lunis.work.mindflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
